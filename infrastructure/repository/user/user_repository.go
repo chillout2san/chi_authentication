e1907665ae4b1b8f86315df30cddfe3ff5fc5f09
chillout2san <chillout2san@gmail.com>
2022-10-27 12:36:12 +0900

    feat: userリポジトリの一部を作成

--- infrastructure/repository/user/user_repository.go
+++ infrastructure/repository/user/user_repository.go
@@ -1,5 +1,12 @@
 package user
 
+import (
+	"chi_sample/common/utils"
+	"chi_sample/infrastructure"
+	"context"
+	"fmt"
+)
+
 type userRepository struct {
 }
 
@@ -7,8 +14,19 @@ func NewAccountRepository() userRepository {
 	return userRepository{}
 }
 
-func (a userRepository) Create(id, name, mail, imagePath, password string) {
+// 新しくユーザーを登録する。
+func (a userRepository) Create(id, name, mail, imagePath, password string) error {
+	sql := `INSERT INTO users(id, name, mail, imagePath, password) VALUE(?,?,?,?,?)`
 
+	hashedPassword := utils.CreateHash(password)
+
+	_, err := infrastructure.Db.ExecContext(context.TODO(), sql, id, mail, imagePath, hashedPassword)
+
+	if err != nil {
+		return fmt.Errorf("infra/create:%v", err)
+	}
+
+	return nil
 }
 
 func (a userRepository) Read() {
