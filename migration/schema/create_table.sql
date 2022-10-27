764d58b6bf280d29f6f7673cedacde7d4900cddc
chillout2san <chillout2san@gmail.com>
2022-10-27 12:35:39 +0900

    feat: migrationのsqlを一部作成

---
+++ migration/schema/create_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE users (
+    id varchar(26) NOT NULL PRIMARY KEY,
+    name varchar(255) NOT NULL,
+    mail varchar(255) NOT NULL,
+    imagePath varchar(255) NOT NULL,
+    pass varchar(255) NOT NULL
+)
