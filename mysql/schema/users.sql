832d692431629a730d34287df238933285d453a8
chillout2san <chillout2san@gmail.com>
2023-02-23 15:01:47 +0900

    ddlのcollate修正

--- mysql/schema/users.sql
+++ mysql/schema/users.sql
@@ -5,4 +5,4 @@ CREATE TABLE `users` (
     `imagePath` varchar(255) NOT NULL COMMENT 'ユーザーのプロフィール写真のパス',
     `pass` varchar(255) NOT NULL COMMENT 'ユーザーのパスワード',
     PRIMARY KEY(`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
