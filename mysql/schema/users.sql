cba69ed77d0e46ec218d5f7bf63dc927431c186d
chillout2san <chillout2san@gmail.com>
2023-03-06 13:54:18 +0900

    docker-composeのファイルを見直した

--- mysql/schema/users.sql
+++ mysql/schema/users.sql
@@ -1,8 +1,9 @@
-CREATE TABLE `users` (
+CREATE TABLE IF NOT EXISTS `users` (
     `id` varchar(26) NOT NULL COMMENT 'ユーザーのid',
     `name` varchar(255) NOT NULL COMMENT 'ユーザーの名前',
     `mail` varchar(255) NOT NULL COMMENT 'ユーザーのメールアドレス',
     `imagePath` varchar(255) NOT NULL COMMENT 'ユーザーのプロフィール写真のパス',
     `pass` varchar(255) NOT NULL COMMENT 'ユーザーのパスワード',
-    PRIMARY KEY(`id`)
+    PRIMARY KEY(`id`),
+    UNIQUE KEY(`mail`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
