aab9ce93fa9a6c12bb76fe6f6d5d081b5db2d955
chillout2san <chillout2san@gmail.com>
2023-05-18 19:41:12 +0900

    cloud buildを使うデプロイ方法に変更

---
+++ deploy.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+PROJECT="learngcp-361805"
+REGION="us-central1"
+
+RED='\e[31m'
+BLUE='\e[34m'
+
+function cancel() {
+    printf "${RED}デプロイを中断します。"
+    exit 1
+}
+
+# mainブランチかどうか確認する
+if [[ $(git rev-parse --abbrev-ref HEAD) != "main" ]]; then
+    printf "${RED}デプロイできるのはmainブランチのみです。"
+    cancel
+fi
+
+# GCPの認証情報があるかどうか確認する
+echo "GCPの認証情報を表示します。"
+echo "******************************"
+gcloud auth list
+echo "******************************"
+
+echo "デプロイしたいGCPプロジェクトに所属するアカウントでしたか？"
+read -p "Y/n> " answer2
+if [[ "${answer2}" == "n" ]]; then
+    printf "${RED}gcloud auth loginでアカウントを切り替えてください。"
+    cancel
+fi
+
+echo "デプロイを開始します。"
+
+# ProjectとRegionを設定する
+gcloud config set project "${PROJECT}"
+gcloud config set run/region "${REGION}"
+
+# コンテナをpushするRegistoryが存在するか確認する
+gcloud artifacts repositories list | grep "chi-authentication"
+if [[ $? == 1 ]]; then
+    printf "${RED}コンテナをpushするArtifact Registryが存在しません。"
+    cancel
+fi
+
+# Cloud Buildでビルドする
+gcloud builds submit --region="${REGION}" --config cloudbuild.yaml
+if [[ $? == 1 ]]; then
+    printf "${RED}コンテナのビルドに失敗しました。"
+    cancel
+fi
+
+printf "${BLUE}デプロイが完了しました。"
--- memo.md
+++ memo.md
@@ -4,6 +4,9 @@ https://cloud.google.com/build/docs/locations?hl=ja#restricted_regions_for_some_
 cloud buildsをyamlで制御できないか考える。
 gcloud builds submit --region=us-central1 --tag us-central1-docker.pkg.dev/learngcp-361805/chi-authentication/backend:latest
 
+Cloud Runデプロイしようとするとサービスアカウントにロールを渡さないといけない。
+https://cloud.google.com/build/docs/deploy-containerized-application-cloud-run?hl=ja
+
 ### デプロイコマンド
 gcloud run deploy chi-authentication --region us-central1 \
 --image us-central1-docker.pkg.dev/learngcp-361805/chi-authentication/backend:latest --allow-unauthenticated \
